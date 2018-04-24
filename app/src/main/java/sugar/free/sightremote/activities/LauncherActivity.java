6190b122df1610c3af77e16691b778d3434b5897
TebbeUbben <t.ubben@holtrop.de>
2018-04-24 14:57:54 +0200

    Connect to service to check if pump has been paired

--- app/src/main/java/sugar/free/sightremote/activities/LauncherActivity.java
+++ app/src/main/java/sugar/free/sightremote/activities/LauncherActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import sugar.free.sightparser.handling.ServiceConnectionCallback;
+import sugar.free.sightparser.handling.SightServiceConnector;
 import sugar.free.sightremote.R;
 
-public class LauncherActivity extends AppCompatActivity {
+public class LauncherActivity extends AppCompatActivity implements ServiceConnectionCallback {
 
     private static final String AUTHORIZE_POLL_EXTRA = "authorize_poll";
+
+    private SightServiceConnector connector;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,11 +22,29 @@ public class LauncherActivity extends AppCompatActivity {
         final Bundle bundle = getIntent().getExtras();
         if ((bundle != null) && (!bundle.getString(AUTHORIZE_POLL_EXTRA, "").equals(""))) {
             startActivity(new Intent(this, AuthorizeActivity.class).putExtra(AUTHORIZE_POLL_EXTRA, bundle.getString(AUTHORIZE_POLL_EXTRA)));
+            finish();
         } else {
-            if (getSharedPreferences("sugar.free.sightremote.services.SIGHTSERVICE", MODE_PRIVATE).contains("DEVICEMAC"))
-                startActivity(new Intent(this, StatusActivity.class));
-            else startActivity(new Intent(this, SetupActivity.class));
+            connector = new SightServiceConnector(this);
+            connector.setConnectionCallback(this);
+            connector.connectToService();
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (connector != null && connector.isConnectedToService()) connector.disconnectFromService();
+    }
+
+    @Override
+    public void onServiceConnected() {
+        if (connector.isUseable()) startActivity(new Intent(this, StatusActivity.class));
+        else startActivity(new Intent(this, SetupActivity.class));
         finish();
+    }
+
+    @Override
+    public void onServiceDisconnected() {
+
     }
 }
